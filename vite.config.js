b0c788a2ab2a8c2ae20f0268baeec4b7569d8560
ffalconet <f_falconet@yahoo.fr>
2024-03-26 10:03:33 +0100

    change static

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '',
+  base: '/test/',
   root:"src",
-  build: {
-    outDir: '../dist',
-  },
   plugins: [vue()],
   server: { fs: { deny: ['.env', '.env.*', '*.{crt,pem}', 'custom.secret'] } }
 })
