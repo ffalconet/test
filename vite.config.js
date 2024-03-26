ebe3b6a0371d3fbe1c71803b65501bba2d357d0e
ffalconet <f_falconet@yahoo.fr>
2024-03-26 09:46:05 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
+  build: {
+    outDir: '../dist',
+  },
   plugins: [vue()],
   server: { fs: { deny: ['.env', '.env.*', '*.{crt,pem}', 'custom.secret'] } }
 })
